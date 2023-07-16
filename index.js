165456c8399b0fa29b26d19294747782f6b0d08b
sharvarihupare-369 <sharvarihupare15@gmail.com>
2023-07-16 16:21:35 +0530

    backend

---
+++ index.js
@@ -0,0 +1,25 @@
+const express =require("express")
+const connection = require("./db")
+const userRouter = require("./routes/userRoute")
+const postRouter = require("./routes/postRoutes")
+const app = express()
+const cors = require("cors")
+require("dotenv").config()
+app.use(express.json())
+app.use(cors())
+app.use("/users",userRouter)
+app.use("/posts",postRouter)
+
+
+
+
+
+app.listen(process.env.port,async()=>{
+    try {
+        await connection
+        console.log("Connected to DB")
+    } catch (error) {
+        console.log(error)
+    }
+    console.log(`Server is running on port ${process.env.port}`)
+})
